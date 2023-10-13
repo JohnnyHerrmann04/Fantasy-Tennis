17353b0124718b2ed02d629d11ab7f7bdbe373b1
Johnny Herrmann <JHerrmann@AccumTech.com>
2023-10-12 23:12:14 -0400

    Javascript executing on page

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿// From Web
 using HtmlAgilityPack;
+using PuppeteerSharp;
 
-var url = "http://html-agility-pack.net/";
-var web = new HtmlWeb();
-var doc = web.Load(url);
-System.Console.WriteLine(doc.DocumentNode.OuterHtml);
+var url = "https://www.espn.com/nfl/boxscore/_/gameId/401547469";
+
+await new BrowserFetcher().DownloadAsync();
+
+using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true }))
+using (var page = await browser.NewPageAsync())
+{
+    await page.GoToAsync(url);
+
+    string htmlContent = await page.GetContentAsync();
+
+    var htmlDoc = new HtmlDocument();
+    htmlDoc.LoadHtml(htmlContent);
+
+    File.WriteAllText("myFile", htmlDoc.DocumentNode.OuterHtml);
+
+    string classNameToSelect = "ScoreCell__Time Gamestrip__Time h9 clr-gray-01"; // Replace with the actual class name
+    HtmlNode divElement = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='" + classNameToSelect + "']");
+    System.Console.WriteLine(divElement.OuterHtml);
+
+}
+
