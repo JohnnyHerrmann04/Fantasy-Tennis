365c236ab16e1414ac05c890bd92521963f563dc
Johnny Herrmann <JHerrmann@AccumTech.com>
2023-10-12 22:25:12 -0400

    Html Parsing working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// From Web
+using HtmlAgilityPack;
+
+var url = "http://html-agility-pack.net/";
+var web = new HtmlWeb();
+var doc = web.Load(url);
+System.Console.WriteLine(doc.DocumentNode.OuterHtml);
